643ccea059c61c439ede9cd85f3a51e1a39df4f3
msrogerio <msrgeri9@gmail.com>
2023-04-26 08:51:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Virgílio
+
+Script comparar duas listas de arquvos .TXT.
+
+### 1. Clonar o reposótio:
+
+Use o comando:
+
+`git clone https://github.com/msrogerio/tantalo.git`
+
+
+### 2. Execute o script:
+
+Na pasta com arquivo .py execute o script com:
+
+`python3 tantalo.py`
+
+Será solicitado o caminho absoluto do aquivo requirements.txt. Use o `pwd` (linux e mac) ou `dir` (windows) para copiar o caminho absoluto do arquivo. 
+
+Dever ser informado algo como:
+
+*/home/meu-usuario/apps/requirements.txt*
+
+
+### Autor
+
+#### Marlon Rogério
+
+[![Twitter Badge](https://img.shields.io/badge/-@MarlonRogrio3-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/MarlonRogrio3)](https://twitter.com/MarlonRogrio3) 
+
+
+<blockquote>
+    Todos os direitos reservados.
+</blockquote>
---
+++ run.py
@@ -0,0 +1,53 @@
+import unidecode 
+
+limpo = ''
+
+with open('nomes.txt', "r") as arquivo:
+    conteudo = arquivo.read()
+    linhas = conteudo.split("\n")
+    for i in linhas:
+        temp = i.split(" 0044.")[0].upper() + "\n"
+        temp = unidecode.unidecode(temp)
+        limpo += temp
+
+with open('nomes.txt', "w") as arquivo:
+    arquivo.write(limpo)
+
+limpo = ''
+
+# -- 
+
+with open('efetivo.txt', "r") as arquivo:
+    conteudo = arquivo.read()
+    linhas = conteudo.split("\n")
+    for i in linhas:
+        temp = i.split(" ")
+        # print(temp)
+        string_nova = ''
+        for j, k in enumerate(temp):
+            if j > 2:
+                string_nova += k + ' '
+                
+        limpo += string_nova.split(" 6º")[0].upper() + "\n"
+
+with open('efetivo-limpo.txt', "w") as arquivo:
+    arquivo.write(limpo)
+
+# --
+arquivo_um = ''
+arquivo_dois = ''
+with open('efetivo-limpo.txt', "r") as arquivo:
+    arquivo_um = arquivo.read()
+
+with open('nomes.txt', "r") as arquivo:
+    arquivo_dois = arquivo.read()
+
+nao_tem_certificados = []
+
+arquivo_um = arquivo_um.split("\n")
+for i in arquivo_um:
+    if not i in arquivo_dois:
+        nao_tem_certificados.append(i)
+
+print(nao_tem_certificados)
+print(len(nao_tem_certificados))
